2fca611ab7ab281e1798b723b46c4c13d10f596e
sercansencanl <77782348+sercansencanl@users.noreply.github.com>
2021-01-23 20:08:54 +0300

    Add files via upload

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> citys = new MyList<string>();
+            citys.Add("Istanbul");
+            citys.Add("Ankara");
+            citys.Add("Izmir");
+            citys.Add("Kocaeli");
+            citys.Add("Kastamonu");
+
+
+            //list i yazdıran method 
+            citys.Write();
+
+            Console.ReadLine();
+        }
+    }
+}
